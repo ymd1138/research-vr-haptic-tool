d7afc586dd65e23e10db053cb6c0bcf02b5aa187
R2D2 <74826010+ymd1138@users.noreply.github.com>
2023-11-09 17:05:09 +0900

    UnityからArduinoへの一方向シリアル通信を実装。タイムアウト処理を追加。

---
+++ Assets/Trigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    //SerialHandler.csのクラス
+    public SerialHandler serialHandler;
+    int i = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Stick")
+        {
+            Debug.Log("Enter");
+            try
+            {
+                serialHandler.Write("1");
+                GetComponent<Renderer>().material.color = Color.green;
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Stick")
+        {
+            Debug.Log("Exit");
+            try
+            {
+                serialHandler.Write("0");
+                GetComponent<Renderer>().material.color = Color.white;
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+}
