5d54d1b230277c70831adc107d2f44eda75fa4e7
R2D2 <74826010+ymd1138@users.noreply.github.com>
2024-01-14 10:47:44 +0900

    棒の先端の点と、根元の点を表示

--- Assets/Script_s6/PointCheck.cs
+++ Assets/Script_s6/PointCheck.cs
@@ -9,17 +9,34 @@ public class PointCheck : MonoBehaviour
 {
     // Stick オブジェクトのスクリプトの値を読み込む
     SpeedMeasure sm;
+    private int sphere;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject obj = GameObject.Find("Stick"); //オブジェクトを探す
         sm = obj.GetComponent<SpeedMeasure>(); //付いているスクリプトを取得
+
+        if (this.gameObject.name == "Sphere1")
+        {
+            sphere = 1;
+        }
+        else if (this.gameObject.name == "Sphere2")
+        {
+            sphere = 2;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = sm.point_P;
+        if (sphere == 1)
+        {
+            transform.position = sm.P_top;
+        }
+        else if (sphere == 2)
+        {
+            transform.position = sm.P_bottom;
+        }
     }
 }
